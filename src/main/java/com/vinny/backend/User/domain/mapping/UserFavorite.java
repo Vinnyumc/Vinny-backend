6c6804fd93489dea63e277ec6264ff05f0f9beb2
kimssirr <kimssirr@naver.com>
2025-07-09 11:50:43 +0900

    feat: user_favorites 엔티티 추가

---
+++ src/main/java/com/vinny/backend/User/domain/mapping/UserFavorite.java
@@ -0,0 +1,31 @@
+package com.vinny.backend.User.domain.mapping;
+
+import com.vinny.backend.Shop.domain.Shop;
+import com.vinny.backend.User.domain.User;
+import com.vinny.backend.common.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "user_favorites", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "shop_id"})
+}) // 유저가 같은 샵을 중복으로 즐겨찾기하지 못하게 막는 제약 조건
+public class UserFavorite extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shop_id", nullable = false)
+    private Shop shop;
+
+}
