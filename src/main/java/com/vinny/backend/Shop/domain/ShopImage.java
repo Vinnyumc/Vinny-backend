fc51b6c0e54f07dc50eb774be1381ef3fad55817
kimssirr <kimssirr@naver.com>
2025-07-10 10:33:10 +0900

    fix: ShopImage H2 문법 오류

--- src/main/java/com/vinny/backend/Shop/domain/ShopImage.java
+++ src/main/java/com/vinny/backend/Shop/domain/ShopImage.java
@@ -24,6 +24,9 @@ public class ShopImage extends BaseEntity {
     @Column(name = "image_url", nullable = false, length = 255)
     private String imageUrl;
 
-    @Column(name = "is_main_image", nullable = false, columnDefinition = "TINYINT(1) DEFAULT 0")
-    private boolean IsMainImage;
+//    @Column(name = "is_main_image", nullable = false, columnDefinition = "TINYINT(1) DEFAULT 0") // MYSQL 용
+//    private boolean IsMainImage;
+    @Column(name = "is_main_image", nullable = false) // H2 용
+    private boolean isMainImage = false;
+
 }
