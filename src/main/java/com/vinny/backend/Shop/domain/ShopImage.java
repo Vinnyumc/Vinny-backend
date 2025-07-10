8f59e123411a2ea9ec8f47ca048a20e7d2001369
kimssirr <kimssirr@naver.com>
2025-07-10 10:35:05 +0900

    fix: 하나로 통합

--- src/main/java/com/vinny/backend/Shop/domain/ShopImage.java
+++ src/main/java/com/vinny/backend/Shop/domain/ShopImage.java
@@ -24,9 +24,7 @@ public class ShopImage extends BaseEntity {
     @Column(name = "image_url", nullable = false, length = 255)
     private String imageUrl;
 
-//    @Column(name = "is_main_image", nullable = false, columnDefinition = "TINYINT(1) DEFAULT 0") // MYSQL 용
-//    private boolean IsMainImage;
-    @Column(name = "is_main_image", nullable = false) // H2 용
+    @Column(name = "is_main_image", nullable = false)
     private boolean isMainImage = false;
 
 }
