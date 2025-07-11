7d27ef6ba2be3b08d00a30093ded382ebdf8b56c
Handongbeom <zzx010319@naver.com>
2025-07-11 12:50:22 +0900

    refactor: SecurityConfig api/post 설정 추가

--- src/main/java/com/vinny/backend/config/SecurityConfig.java
+++ src/main/java/com/vinny/backend/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
-                                "/api/auth/**", "/h2-console/**", "/api/shop/**").permitAll() // 로그인 및 H2 콘솔 경로는 모두 허용
+                                "/api/auth/**", "/h2-console/**", "/api/shop/**","/api/post/**").permitAll() // 로그인 및 H2 콘솔 경로는 모두 허용
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
