8d58368e633bc0249002676f5177ff7b8d4a9ac5
OhDG <ohdonggyu@naver.com>
2025-07-25 17:13:31 +0900

    fix: SearchLogService 에러 해결

--- src/main/java/com/vinny/backend/search/service/SearchLogService.java
+++ src/main/java/com/vinny/backend/search/service/SearchLogService.java
@@ -41,8 +41,6 @@ public class SearchLogService {
     public SearchLogResponse addSearchLog(Long userId, SearchLogCreateRequest request) {
         // 사용자 조회
         User user = userRepository.findById(userId)
-      
-        User user = userRepository.findById(userId)
                 .orElseThrow(() -> new GeneralException(ErrorStatus.USER_NOT_FOUND));
 
         // 키워드 정제 (앞뒤 공백 제거, 소문자 변환 등)
