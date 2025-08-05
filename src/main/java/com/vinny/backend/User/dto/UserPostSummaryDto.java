04dae640fe77d5bd2c6f91b9e0f453be1245b2a2
Handher0 <handher0@naver.com>
2025-08-05 15:51:45 +0900

    feat:상대방 프로필, 게시글 조회

---
+++ src/main/java/com/vinny/backend/User/dto/UserPostSummaryDto.java
@@ -0,0 +1,10 @@
+package com.vinny.backend.User.dto;
+
+import java.time.LocalDateTime;
+
+public record UserPostSummaryDto(
+    Long postId,
+    String content,
+    String imageUrl,
+    LocalDateTime createdAt
+) {}
