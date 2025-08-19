8c1d1850ad5cd58413733dedd3576a7507b528a7
Handher0 <150361814+handher0@users.noreply.github.com>
2025-08-20 01:43:47 +0900

    refactor:상대방 프로필 조회시 프로필, 배경이미지 포함 (#236)

--- src/main/java/com/vinny/backend/User/dto/UserProfileDto.java
+++ src/main/java/com/vinny/backend/User/dto/UserProfileDto.java
@@ -9,6 +9,8 @@ public class UserProfileDto {
     private String comment;
     private int postCount;
     private int bookmarkCount;
+    private String profileImageUrl;
+    private String backgroundImageUrl;
 
     public UserProfileDto(Long userId, String nickname, String comment) {
         this.userId = userId;
@@ -16,12 +18,14 @@ public class UserProfileDto {
         this.comment = comment;
     }
 
-    public UserProfileDto(Long userId, String nickname, String comment, int postCount, int bookmarkCount) {
+    public UserProfileDto(Long userId, String nickname, String comment, int postCount, int bookmarkCount, String profileImageUrl, String backgroundImageUrl) {
         this.userId = userId;
         this.nickname = nickname;
         this.comment = comment;
         this.postCount = postCount;
         this.bookmarkCount = bookmarkCount;
+        this.profileImageUrl = profileImageUrl;
+        this.backgroundImageUrl = backgroundImageUrl;
     }
 
 }
