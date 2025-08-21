5ff2b4526cb675543568e661dada104a1c8e3e39
Handher0 <handher0@naver.com>
2025-08-21 22:42:28 +0900

    fix:이미지 중복 해결

--- src/main/java/com/vinny/backend/post/repository/PostRepository.java
+++ src/main/java/com/vinny/backend/post/repository/PostRepository.java
@@ -90,13 +90,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("""
         SELECT DISTINCT p FROM Post p
         LEFT JOIN FETCH p.user u
-        LEFT JOIN FETCH p.images i
-        LEFT JOIN FETCH p.brandHashtags bh
-        LEFT JOIN FETCH bh.brand b
-        LEFT JOIN FETCH p.shopHashtags sh
-        LEFT JOIN FETCH sh.shop s
-        LEFT JOIN FETCH p.styleHashtags sth
-        LEFT JOIN FETCH sth.vintageStyle vs
         WHERE p.id IN :ids
     """)
     List<Post> findAllWithAssociationsByIdIn(@Param("ids") List<Long> ids);
